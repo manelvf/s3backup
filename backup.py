de76d411e0523cdcf3ebb052e2c53e2bbd7e8a4c
Chuza <chuza.gl@gmail.com>
2016-02-08 22:14:28 +0000

    added support for multiple files

--- backup.py
+++ backup.py
@@ -13,17 +13,21 @@ def connect_to_s3():
 
 def run(bucket):
     k = Key(bucket)
-    k.key = settings["prefix"] + uuid4().hex
-    k.set_contents_from_filename(settings["backup-file"], reduced_redundancy=True)
-    print("file {} uploaded".format(k.key))
+    for fn in settings["backup-files"]:
+        k.key = settings["prefix"] + os.path.basename(fn) + '-' + uuid4().hex
+        k.set_contents_from_filename(
+            fn,
+            reduced_redundancy=settings["S3"]["RR"])
+        print("File {} uploaded".format(k.key))
 
 
 if __name__ == "__main__":
     with open(
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings.json")
+            os.path.join(
+                 os.path.dirname(os.path.realpath(__file__)), "settings.json")
         ) as json_data_file:
         settings = json.load(json_data_file)
 
     bucket = connect_to_s3()
-    print("Location: {}".format(bucket.get_location())
+    print("Location: {}".format(bucket.get_location()))  # debug line
     run(bucket)
